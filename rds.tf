586711aaab2358c6220496fb445ba874b28fda0a
Nombulelo Khumalo <kumzalelo23@gmail.com>
2025-03-19 20:20:07 +0000

    Update PostgreSQL engine version to 14.17

--- rds.tf
+++ rds.tf
@@ -5,9 +5,9 @@ resource "aws_db_instance" "postgres" {
   allocated_storage       = 20
   storage_type            = "gp2"
   engine                  = "postgres"
-  engine_version          = "14.5"
+  engine_version          = "14.17"
   instance_class          = var.db_instance_class
-  name                    = var.db_name
+  db_name                 = var.db_name
   username                = var.db_username
   password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
